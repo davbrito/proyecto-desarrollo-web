0d648fa392a10385e71ecf5c8dc379e7ffcf5196
jdgm464 <jdgm464@gmail.com>
2025-12-09 23:10:41 -0400

    chore: remove .env.example file and add console logs for DATABASE_URL and REDIS_URL in database configuration

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -5,8 +5,12 @@ const __dirname = import.meta.dirname;
 
 export const databaseConfig = registerAs(
   "database",
-  () =>
-    ({
+  () => {
+    // Agregar el console.log aquí, dentro de la función
+    console.log("DATABASE_URL:", process.env.DATABASE_URL);
+    console.log("REDIS_URL:", process.env.REDIS_URL);
+    
+    return {
       type: "postgres",
       url: process.env.DATABASE_URL,
       entities: [__dirname + "/../**/*.entity{.ts,.js,.mjs}"],
@@ -16,5 +20,6 @@ export const databaseConfig = registerAs(
         type: "ioredis",
         options: process.env.REDIS_URL,
       },
-    }) satisfies TypeOrmModuleOptions,
+    } satisfies TypeOrmModuleOptions;
+  },
 );
