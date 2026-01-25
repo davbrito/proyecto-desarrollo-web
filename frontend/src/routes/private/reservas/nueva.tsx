c81636c3d5e7539465c458c54ad15313459df4aa
Lorenzo Parra <alejandro36036@gmail.com>
2026-01-25 15:22:57 -0400

    feature: implementation of endpoint
     implementation of endpoint  of reservation numbers

--- frontend/src/routes/private/reservas/nueva.tsx
+++ frontend/src/routes/private/reservas/nueva.tsx
@@ -1,6 +1,5 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import ReservationForm from "@/components/reservas/reservation-form";
-import { AvailableHours, AvailableLaboratorys } from "@/components/reservas/reservation-form/schema";
+import { AvailableHours } from "@/components/reservas/reservation-form/schema";
 import type { Route } from "./+types/nueva";
 import { useEffect } from "react";
 import { getAccessToken } from "@/lib/auth";
@@ -9,72 +8,100 @@ import { useState } from "react";
 export async function clientLoader() {
   return {
     availableHours: AvailableHours,
-    availableLaboratory: AvailableLaboratorys,
   };
 }
 
-
 export default function NuevaReserva({ loaderData }: Route.ComponentProps) {
-  const [laboratorys, setLaboratorys] = useState<string[]>([]);
-  const [stateEventType, setEventType] = useState<any[]>([]);
-  
-  
-  useEffect( () => {
-     
-     const fetchDataLaboratory = async () => {
-    const token = await getAccessToken();
+  const [laboratorys, setLaboratorys] = useState([]);
+  const [stateEventType, setEventType] = useState([]);
+  const [reserved, sethoursReserved] = useState([]);
 
-    console.log(token)
-    try {
-      const res = await fetch('http://localhost:3001/api/laboratories', {
-        method: 'GET', 
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}` 
+  useEffect(() => {
+    const fetchDataLaboratory = async () => {
+      const token = await getAccessToken();
+
+      try {
+        const res = await fetch("http://localhost:3000/api/laboratories", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (!res.ok) {
+          throw new Error(`Error: ${res.status}`);
         }
-      });
 
-      if (!res.ok) {
-        throw new Error(`Error: ${res.status}`);
+        const data = await res.json();
+
+        setLaboratorys(data);
+      } catch (error) {
+        console.error("Error al obtener laboratorios:", error);
       }
+    };
 
-      const data = await res.json();
+    const fetchDataTypeActivity = async () => {
+      const token = await getAccessToken();
+      try {
+        const res = await fetch("http://localhost:3000/api/reserve-types", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
 
-      setLaboratorys(data)
-      console.log("pene", token);
-    } catch (error) {
-      console.error("Error al obtener laboratorios:", error);
-    }
-  };
-
- const fetchDataTypeActivity = async () => {
-    const token = await getAccessToken();
-    try {
-      const res = await fetch('http://localhost:3001/api/reserve-types', {
-        method: 'GET', 
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}` 
+        if (!res.ok) {
+          throw new Error(`Error: ${res.status}`);
         }
-      });
 
-      if (!res.ok) {
-        throw new Error(`Error: ${res.status}`);
+        const data = await res.json();
+
+        setEventType(data);
+      } catch (error) {
+        console.error("Error al obtener laboratorios:", error);
       }
+    };
 
-      const data = await res.json();
+    const reservedHours = async () => {
+      const token = await getAccessToken();
+      try {
+        const res = await fetch("http://localhost:3000/api/reservations", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
 
-      setEventType(data)
-    } catch (error) {
-      console.error("Error al obtener laboratorios:", error);
-    }
-  };
-  
-  fetchDataTypeActivity()
-  fetchDataLaboratory();
-}, []);
+        if (!res.ok) {
+          throw new Error(`Error: ${res.status}`);
+        }
 
+        const data = await res.json();
 
+        const reserved = data.map((reser: any) => ({
+          startDate: reser.startDate,
+          defaultStartTime: reser.defaultStartTime,
+        }));
+        sethoursReserved(reserved);
+      } catch (error) {
+        console.error("Error al obtener laboratorios:", error);
+      }
+    };
 
-  return <ReservationForm availableHours={loaderData.availableHours} availableLaboratory={laboratorys} stateTypeEvent={stateEventType} />;
+    reservedHours();
+    fetchDataTypeActivity();
+    fetchDataLaboratory();
+  }, []);
+
+  return (
+    <ReservationForm
+      availableHours={loaderData.availableHours}
+      availableLaboratory={laboratorys}
+      stateTypeEvent={stateEventType}
+      reserved={reserved}
+    />
+  );
 }
